37da72ea949eb1dba7f59987178772cd2d90120b
loun <lounas1994@gmail.com>
2022-11-26 21:11:04 +0100

    fix : change chkra ui default colors and button props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,38 @@
 import '../styles/globals.css'
+import { chakraUi } from 'chakra'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  color: {
+    red: {
+      900: '#510B0B'
+    },
+    gray: {
+      200: '#EAEAEA',
+      700: '#2D2D2D'
+    },
+    yellow: {
+      300: '#F4DE4D'
+    }
+  },
+  variants: {
+    Button: {
+      variants: {
+        link: {
+          ':focus': {
+            outline: 'none',
+            boxShadow: 'none'
+          }
+        }
+      }
+    }
+  }
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  ;<ChakraProvider theme={theme}>
+    return <Component {...pageProps} />
+  </ChakraProvider>
 }
 
 export default MyApp
