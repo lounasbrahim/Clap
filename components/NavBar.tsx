3f7326fba085d341cded60d4bd170515bd54abca
loun <lounas1994@gmail.com>
2022-11-29 14:44:48 +0100

    feat : add navBar component

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,24 +1,45 @@
-import { Box, HStack } from '@chakra-ui/layout'
+import { Box, HStack, Flex, Spacer } from '@chakra-ui/layout'
 import NextImage from 'next/image'
+import { Input } from '@chakra-ui/react'
 
 const NavBar = () => {
   return (
-    <Box>
-      <HStack>
-        <NextImage
-          alt="next page"
-          src="/icons/prevPage.svg"
-          width={50}
-          height={50}
-        />
-        <NextImage
-          alt="next page"
-          src="/icons/nextPage.svg"
-          width={50}
-          height={50}
-        />
-      </HStack>
-    </Box>
+    <Flex mr="30px">
+      <Box>
+        <HStack>
+          <NextImage
+            alt="next page"
+            src="/icons/prevPage.svg"
+            width={40}
+            height={40}
+          />
+          <NextImage
+            alt="next page"
+            src="/icons/nextPage.svg"
+            width={40}
+            height={40}
+          />
+        </HStack>
+      </Box>
+      <Spacer />
+      <Box>
+        <HStack>
+          <NextImage
+            alt="search"
+            src="/icons/search.svg"
+            width={35}
+            height={35}
+          />
+          <Input placeholder="Search music" border="none" />
+          <NextImage
+            alt="account"
+            src="/icons/account.svg"
+            width={35}
+            height={35}
+          />
+        </HStack>
+      </Box>
+    </Flex>
   )
 }
 
